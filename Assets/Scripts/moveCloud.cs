84eda8e91769fcebee6d7295c89cfbb8ee6e51aa
Evgeny Suetin <evgenysuetin@kilograpp.com>
2016-05-14 20:26:39 +0400

    first commit

---
+++ Assets/Scripts/moveCloud.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCloud : MonoBehaviour {
+
+
+    //false - right
+    //true - left
+    public bool side = false;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (side)
+        {
+            transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
+        }
+    }
+}
